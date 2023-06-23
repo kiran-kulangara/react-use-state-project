3267be8f57ec9ebf42cf0e2735f2786c6b7e92ec
Kiran Joy Kulangara <kirankulangara.mec@gmail.com>
2023-06-23 17:48:10 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import UseStateBasics from "../src/components/useStateBasics.jsx";
+
+function App() {
+  return (
+    <div className="container">
+      <h2>Hello world</h2>
+      <UseStateBasics />
+    </div>
+  );
+}
+
+export default App;
